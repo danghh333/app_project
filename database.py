dfc35917d0dbe3010dea52262064ba00771c0439
danghh333 <dang032003@gmail.com>
2023-03-25 15:20:49 +0700

    Added my project

---
+++ database.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+def add_printer(name, manufacturer, model, serial_num, firmware_ver, calibration, date):
+    """Add all the information of the 3D printer machine"""
+    con = sqlite3.connect('3d_printer_machine_management.db')
+    cur = con.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS printer (name TEXT, manufacturer TEXT, model TEXT, serial_num INTEGER, firmware_vers TEXT, calibration TEXT, date DATE)')
+    con.commit()
+    con.close()
